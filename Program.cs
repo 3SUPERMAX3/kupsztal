154ae7b693c54f6a260312c0917c2774524b23fd
3SUPERMAX3 <101880494+3SUPERMAX3@users.noreply.github.com>
2023-11-05 09:15:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace polimorfizm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
